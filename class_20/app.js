cdf334974066e82f69ecab21bbbe07c83dc85ab4
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2024-12-17 12:03:03 +0500

    class_20

---
+++ class_20/app.js
@@ -0,0 +1,29 @@
+                   // nested for loop 
+// for (var i=0; i<3; i++){
+
+//   for(var j=0; j<2; j++){
+//     console.log("muzammil");
+//   }
+// }
+                  // print all elements one by one
+// var array=[[1,2,3],["a","b","c"]];
+// for(var i=0;i<array.length;i++){
+// for (var j=0; j<array[i].length;j++){
+// console.log(array[i][j]);
+// }
+// }
+/*                  // star pattern
+*
+**
+***
+****
+*****
+*/
+// for(var i=1; i<=5;i++){
+//     var star= " ";
+//     for(var j=1;j<=i;j++){
+//         star= star +" *"
+//     }
+//     console.log(star);
+    
+// }
